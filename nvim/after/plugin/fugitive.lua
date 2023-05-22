dc637a184007650f420e2dfe09675a39b2a1b642
Marcus <marcuscchan@gmail.com>
2023-05-22 16:58:40 -0700

    Added submodule

--- nvim/after/plugin/fugitive.lua
+++
@@ -1,3 +0,0 @@
-vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
-vim.keymap.set("n", "<leader>p", function() vim.cmd.Git("push") end)
-vim.keymap.set("n", "<leader>pa", function() vim.cmd.Git("push -u origin") end)
