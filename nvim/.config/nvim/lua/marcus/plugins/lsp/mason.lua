1daee8bc43d008370dd13f968cdd63151a6ca3f0
Marcus Chan <marcuscchan@gmail.com>
2025-09-16 19:50:40 -0700

    fix douple lsp nvim

--- nvim/.config/nvim/lua/marcus/plugins/lsp/mason.lua
+++ nvim/.config/nvim/lua/marcus/plugins/lsp/mason.lua
@@ -1,58 +1,35 @@
 return {
 	"williamboman/mason.nvim",
 	dependencies = {
-		"williamboman/mason-lspconfig.nvim",
 		"WhoIsSethDaniel/mason-tool-installer.nvim",
 	},
 	config = function()
 		local mason = require("mason")
-		local masonLspConfig = require("mason-lspconfig")
 
 		mason.setup({})
 
 		local mason_tool_installer = require("mason-tool-installer")
 		mason_tool_installer.setup({
 			ensure_installed = {
+				-- Formatters and linters
 				"prettier", -- prettier formatter
 				"stylua", -- lua formatter
 				"eslint_d", -- js linter
 				"clang-format",
+				-- LSP servers (installed but not auto-configured)
+				"typescript-language-server", -- ts_ls
+				"html-lsp", -- html
+				"css-lsp", -- cssls
+				"tailwindcss-language-server", -- tailwindcss
+				"lua-language-server", -- lua_ls
+				"prisma-language-server", -- prismals
+				"ruff", -- ruff (same name)
+				"gopls", -- gopls (same name)
+				"templ", -- templ (same name)
+				"clangd", -- clangd (same name)
+				"pyright", -- pyright (same name)
+				"cmake-language-server", -- cmake
 			},
 		})
-
-		masonLspConfig.setup({
-			ensure_installed = {
-				"ts_ls",
-				"html",
-				"cssls",
-				"tailwindcss",
-				"lua_ls",
-				"prismals",
-				"ruff",
-				"gopls",
-				"templ",
-				"clangd",
-				"pyright",
-				"cmake",
-			},
-			automatic_installation = false,
-		})
-
-		-- masonLspConfig.setup_handlers({
-		-- 	-- Will be called for each installed server that doesn't have
-		-- 	-- a dedicated handler.
-		-- 	--
-		-- 	function(server_name) -- default handler (optional)
-		-- 		-- https://github.com/neovim/nvim-lspconfig/pull/3232
-		-- 		if server_name == "tsserver" then
-		-- 			server_name = "ts_ls"
-		-- 		end
-		-- 		local capabilities = require("cmp_nvim_lsp").default_capabilities()
-		-- 		require("lspconfig")[server_name].setup({
-		--
-		-- 			capabilities = capabilities,
-		-- 		})
-		-- 	end,
-		-- })
 	end,
 }
