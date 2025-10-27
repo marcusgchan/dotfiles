4b891138eedda19c4ed23aade05487babf4902b6
Marcus Chan <marcuscchan@gmail.com>
2025-10-26 20:58:17 -0700

    separate linux an d mac

--- run.sh
+++ run.sh
@@ -1,16 +1,14 @@
 #!/usr/bin/env bash
 
 macos=("aerospace" "ghostty" "nvim" "tmux" "scripts")
+linux=("ghostty" "nvim" "tmux" "scripts" "i3" "kitty" "waybar" "hypr")
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
   for pkg in "${macos[@]}"; do
     stow -vv -t ~ "$pkg"
   done
 else
-  for pkg in */; do
-    pkg="${pkg%/}"  # remove trailing slash
-    if [[ ! " ${macos[*]} " =~ " $pkg " ]]; then
-      stow -vv -t ~ "$pkg"
-    fi
+  for pkg in "${linux[@]}"; do
+    stow -vv -t ~ "$pkg"
   done
 fi
