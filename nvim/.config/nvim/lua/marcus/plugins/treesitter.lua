51250326b03ac8c0db0bd57df6998935a855fe29
Marcus Chan <marcuscchan@gmail.com>
2026-04-25 18:21:42 -0700

    update treesitter and telescope

--- nvim/.config/nvim/lua/marcus/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/marcus/plugins/treesitter.lua
@@ -1,40 +1,43 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
+	branch = "main",
 	build = ":TSUpdate",
-	enabled = true,
 	config = function()
-		local configs = require("nvim-treesitter.configs")
+		require("nvim-treesitter").install({
+			"c",
+			"cpp",
+			"cmake",
+			"lua",
+			"vimdoc",
+			"javascript",
+			"typescript",
+			"tsx",
+			"sql",
+			"html",
+			"css",
+			"dockerfile",
+			"json",
+			"prisma",
+			"svelte",
+			"templ",
+			"python",
+			"markdown",
+			"markdown_inline",
+			"hcl",
+			"java",
+		})
 
-		configs.setup({
-			ensure_installed = {
-				"c",
-				"cpp",
-				"cmake",
-				"lua",
-				"vimdoc",
-				"javascript",
-				"typescript",
-				"tsx",
-				"sql",
-				"html",
-				"css",
-				"dockerfile",
-				"json",
-				"prisma",
-				"svelte",
-				"templ",
-				"python",
-				"markdown",
-				"markdown_inline",
-				"hcl",
-				"java",
-			},
-			sync_install = false,
-			highlight = { enable = true },
-			indent = {
-				enable = true,
-				disable = { "typescript", "tsx" },
-			},
+		vim.api.nvim_create_autocmd("FileType", {
+			callback = function(args)
+				local lang = vim.treesitter.language.get_lang(args.match)
+				-- Safely start Treesitter highlighting
+				if lang and pcall(vim.treesitter.start, args.buf, lang) then
+					-- Enable Treesitter indentation (disabled for typescript/tsx)
+					if not vim.tbl_contains({ "typescript", "tsx" }, args.match) then
+						vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+					end
+				end
+			end,
 		})
 	end,
 }
